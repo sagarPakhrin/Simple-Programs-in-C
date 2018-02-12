5a6658dc076ed518f1f84d96ac956e40261fb28b
Sagar Lama <31485851+sagarPakhrin@users.noreply.github.com>
2018-02-12 19:20:08 +0545

    Update digitalClock.c

--- digitalClock.c
+++ digitalClock.c
@@ -8,22 +8,22 @@ int main(){
 	int hour=0,min=0,sec=0;
 	printf("\n\n\n\n");
 	while(1){	// runs an infinite loop
-	printf("\r \t\t\t\t %2d : %2d : %2d ",hour,min,sec);// prints time n the format HH:MM:SS \r updates the code in the same line
-	sec++;
-	fflush(stdout);// flushes buffer in gcc
-	if(sec==60){
-		min++;
-		sec=0;
-	} 
-	if(min==60){
-		hour++;
-		min=0;
-	}
-	if (hour==24){
-		hour=0;
-		min=0;
-		sec=0;
-	}
-	sleep(1); //halts the system for 1s
+		printf("\r \t\t\t\t %2d : %2d : %2d ",hour,min,sec);// prints time n the format HH:MM:SS \r updates the code in the same line
+		sec++;
+		fflush(stdout);// flushes buffer in gcc
+		if(sec==60){
+			min++;
+			sec=0;
+		} 
+		if(min==60){
+			hour++;
+			min=0;
+		}
+		if (hour==24){
+			hour=0;
+			min=0;
+			sec=0;
+		}
+		sleep(1); //halts the system for 1s
 	} 
 }
